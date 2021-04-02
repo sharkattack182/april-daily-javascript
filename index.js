4906dcb1f3aec5a17a5cd199c3c0ad58c818dfb1
Matthew Courtney <matthewdc182@gmail.com>
2021-04-01 20:09:50 -0400

    new month new file

---
+++ index.js
@@ -0,0 +1,5 @@
+// Balanced Words
+// We can assign a value to each character in a word, based on their position in the alphabet (a = 1, b = 2, ... , z = 26). A balanced word is one where the sum of values on the left-hand side of the word equals the sum of values on the right-hand side. For odd length words, the middle character (balance point) is ignored.
+// Difficulty: Very Hard
+// Date: April 1 2021
+// Solve Time: 
