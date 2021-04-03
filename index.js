1b85a8a6c3296241c772c31128ae718b6fdb599a
Matthew Courtney <matthewdc182@gmail.com>
2021-04-02 23:33:17 -0400

    added in daily js problem

--- index.js
+++ index.js
@@ -4,57 +4,65 @@
 // Date: April 1 2021
 // Solve Time: 18 minutes
 
-function balanced(string) {
-    var l = string.length;
+// function balanced(string) {
+//     var l = string.length;
 
-    if(l % 2 == 0) {
-        var half = string.length / 2;
-        var firstHalf = string.substring(0, half);
-        var secondHalf = string.substring(half);
+//     if(l % 2 == 0) {
+//         var half = string.length / 2;
+//         var firstHalf = string.substring(0, half);
+//         var secondHalf = string.substring(half);
 
-        var side1 = 0;
-        var side2 = 0;
+//         var side1 = 0;
+//         var side2 = 0;
         
-        firstHalf.split("").forEach(element => {
-            var value = element.charCodeAt(0) - 96;
-            side1 += value;
-        });
+//         firstHalf.split("").forEach(element => {
+//             var value = element.charCodeAt(0) - 96;
+//             side1 += value;
+//         });
 
-        secondHalf.split("").forEach(element => {
-            var value = element.charCodeAt(0) - 96;
-            side2 += value;
-        });
+//         secondHalf.split("").forEach(element => {
+//             var value = element.charCodeAt(0) - 96;
+//             side2 += value;
+//         });
 
-        if(side1 === side2) {
-            console.log("true")
-        } else {
-            console.log("false")
-        }
-    } else {
-        var mid = string.length / 2 + .5;
-        var firstHalf = string.substring(0, mid - 1);
-        var secondHalf = string.substring(mid);
+//         if(side1 === side2) {
+//             console.log("true")
+//         } else {
+//             console.log("false")
+//         }
+//     } else {
+//         var mid = string.length / 2 + .5;
+//         var firstHalf = string.substring(0, mid - 1);
+//         var secondHalf = string.substring(mid);
 
-        var side1 = 0;
-        var side2 = 0;
+//         var side1 = 0;
+//         var side2 = 0;
         
-        firstHalf.split("").forEach(element => {
-            var value = element.charCodeAt(0) - 96;
-            side1 += value;
-        });
+//         firstHalf.split("").forEach(element => {
+//             var value = element.charCodeAt(0) - 96;
+//             side1 += value;
+//         });
 
-        secondHalf.split("").forEach(element => {
-            var value = element.charCodeAt(0) - 96;
-            side2 += value;
-        });
+//         secondHalf.split("").forEach(element => {
+//             var value = element.charCodeAt(0) - 96;
+//             side2 += value;
+//         });
 
-        if(side1 === side2) {
-            console.log("true")
-        } else {
-            console.log("false")
-        }
-    }
-}
+//         if(side1 === side2) {
+//             console.log("true")
+//         } else {
+//             console.log("false")
+//         }
+//     }
+// }
 
-balanced("zips");
-balanced("brake");
+// balanced("zips");
+// balanced("brake");
+
+
+
+// Product of Digits of Sum
+// Create a function that takes numbers as arguments, adds them together, and returns the product of digits until the answer is only 1 digit long.
+// Difficulty: Very Hard
+// Date: April 2 2021
+// Solve Time: 
