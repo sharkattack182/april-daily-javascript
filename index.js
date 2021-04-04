c590aaf05763ebf64b8d5819c41018960883f883
Matthew Courtney <matthewdc182@gmail.com>
2021-04-04 19:32:22 -0400

    finsished in about 18 minutes had to do some tricky stuff with the spread operator in the second for of loop

--- index.js
+++ index.js
@@ -14,7 +14,7 @@
 
 //         var side1 = 0;
 //         var side2 = 0;
-        
+
 //         firstHalf.split("").forEach(element => {
 //             var value = element.charCodeAt(0) - 96;
 //             side1 += value;
@@ -37,7 +37,7 @@
 
 //         var side1 = 0;
 //         var side2 = 0;
-        
+
 //         firstHalf.split("").forEach(element => {
 //             var value = element.charCodeAt(0) - 96;
 //             side1 += value;
@@ -59,14 +59,11 @@
 // balanced("zips");
 // balanced("brake");
 
-
-
 // Product of Digits of Sum
 // Create a function that takes numbers as arguments, adds them together, and returns the product of digits until the answer is only 1 digit long.
 // Difficulty: Very Hard
 // Date: April 2 2021
 // Solve Time: 19 minutes
-
 
 // function sumDigProd(...nums) {
 //     const sum = nums.reduce((total, num) => total += num, 0);
@@ -89,9 +86,7 @@
 //         }
 //     }
 
-    
 // }
-
 
 // sumDigProd(16, 28);
 // // 16 + 28 = 44
@@ -102,10 +97,32 @@
 
 // sumDigProd(1, 2, 3, 4, 5, 6);
 
-
-
 // Find All Digits
 // Taking each four digit number of an array in turn, return the number that you are on when all of the digits 0-9 have been discovered. If not all of the digits can be found, return "Missing digits!".
 // Difficulty: Very Hard
 // Date: April 3 2021
-// Solve Time: 
+// Solve Time: 18 minutes
+
+function findAllDigits(array) {
+    let found = new Set();
+    for (let n of array) {
+        for(let d of [...`${n}`]) {
+            found.add(d);
+            if(found.size === 10) {
+                console.log(n);
+                return
+            }
+        }
+    }
+    console.log("missing digits")
+}
+
+findAllDigits([5175, 4538, 2926, 5057, 6401, 4376, 2280, 6137, 8798, 9083]);
+// digits found:  517-  4-38  29-6  -0
+
+findAllDigits([5719, 7218, 3989, 8161, 2676, 3847, 6896, 3370, 2363, 1381]);
+// digits found:  5719  -2-8  3---  --6-  ----  --4-  ----  ---0
+
+findAllDigits([4883, 3876, 7769, 9846, 9546, 9634, 9696, 2832, 6822, 6868]);
+// digits found:  48-3  --76  ---9  ----  -5--  ----  ----  2---
+// 0 and 1 are missing
